814b00755f2a5bc63f2ed98a89d5172dbea006fa
Kawtar <Kawtar.shaimi8@gmail.com>
2025-02-27 12:23:55 +0000

    Ajouter model Ticket

--- TicketFlow/app/Models/Ticket.php
+++ TicketFlow/app/Models/Ticket.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    
+    protected $fillable = [
+        'name',
+        'description',
+        'status',
+        'priority',
+        'user_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
