c3e0dc4e4d860cb56e394927a128a3f6bdf1bffe
Kawtar <Kawtar.shaimi8@gmail.com>
2025-02-27 14:21:36 +0000

    Ajouter migration Asignements

---
+++ TicketFlow/database/migrations/2025_02_27_142103_create_asignements_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('asignements', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('ticket_id');
+            $table->foreign('ticket_id')->references('id')->on('tickets')->onDelete('cascade');
+            $table->unsignedBigInteger('developer_id');
+            $table->foreign('developer_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('admin_id')->nullable();
+            $table->foreign('admin_id')->references('id')->on('users')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('asignements');
+    }
+};
