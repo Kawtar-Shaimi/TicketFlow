777c5a693b9ebb4d97037903944fd042bb32c50b
Kawtar <Kawtar.shaimi8@gmail.com>
2025-02-27 14:35:04 +0000

    Ajouter quelques routes

--- TicketFlow/routes/web.php
+++ TicketFlow/routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+/* Auth routes */
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'loginView'])->name('loginView');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::get('/register', [AuthController::class, 'registerView'])->name('registerView');
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+});
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
