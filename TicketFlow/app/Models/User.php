92878cb766c17e5d291834cf44a0bc6580ca717f
Kawtar <Kawtar.shaimi8@gmail.com>
2025-02-27 12:22:58 +0000

    Ajouter model User

---
+++ TicketFlow/app/Models/User.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use PHPUnit\Framework\Attributes\Ticket;
+
+class User extends Authenticatable
+{
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'remember_token'
+    ];
+
+    public function ticket()
+    {
+        return $this->hasMany(Ticket::class);
+    }
+}
